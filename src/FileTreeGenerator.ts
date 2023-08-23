73642928a174edc27d7c7b0d159b63f2d626366c
Unarray <48868686+Unarray@users.noreply.github.com>
2023-08-23 15:48:16 +0200

    Set `FileTreeGenerator` as `Singleton`

--- src/FileTreeGenerator.ts
+++ src/FileTreeGenerator.ts
@@ -1,8 +1,24 @@
 import { GenerateTree } from "#/commands/GenerateTree";
-import type { Command } from "obsidian";
+import type { App, Command, PluginManifest } from "obsidian";
 import { Plugin } from "obsidian";
 
 export default class FileTreeGenerator extends Plugin {
+
+  private static instance: FileTreeGenerator;
+
+  private static setInstance = (instance: FileTreeGenerator): void => {
+    FileTreeGenerator.instance = instance;
+  };
+
+  public static getInstance = (): FileTreeGenerator => {
+    return FileTreeGenerator.instance;
+  };
+
+  constructor(app: App, manifest: PluginManifest) {
+    super(app, manifest);
+    FileTreeGenerator.setInstance(this);
+  }
+
 
   onload(): void {
     this.addCommands(
