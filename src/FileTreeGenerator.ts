5877ff53ee44e9c2fa0f3bb5767dd5b88deacf81
Unarray <48868686+Unarray@users.noreply.github.com>
2023-08-23 15:44:26 +0200

    Register new `GenerateTree` command

--- src/FileTreeGenerator.ts
+++ src/FileTreeGenerator.ts
@@ -1,6 +1,19 @@
+import { GenerateTree } from "#/commands/GenerateTree";
+import type { Command } from "obsidian";
 import { Plugin } from "obsidian";
 
 export default class FileTreeGenerator extends Plugin {
 
+  onload(): void {
+    this.addCommands(
+      new GenerateTree()
+    );
+  }
+
+  public addCommands = (...commands: Command[]): void => {
+    for (const command of commands) {
+      this.addCommand(command);
+    }
+  };
 
 }
