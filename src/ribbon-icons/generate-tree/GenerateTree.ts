87177353f91eddae1e5840891349766e97dd0d8a
Unarray <48868686+Unarray@users.noreply.github.com>
2023-08-26 22:31:59 +0200

    Make ribbon icon only usable in editor

--- src/ribbon-icons/generate-tree/GenerateTree.ts
+++ src/ribbon-icons/generate-tree/GenerateTree.ts
@@ -7,11 +7,22 @@ export class GenerateTree implements RibbonIcon {
 
   public readonly icon: RibbonIconIcon = "folder-tree";
 
-  public readonly title: RibbonIconTitle = "Generate tree folder";
+  public readonly title: RibbonIconTitle = "Generate file tree";
 
   public execute = (): void => {
     const editor = FileTreeGenerator.getInstance().getEditor();
+
     if (!editor) {
+      new Notice("❌ Can only be used during editing");
+      return;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const viewType = FileTreeGenerator.getInstance().app.workspace.activeEditor.currentMode.type as string;
+
+    if (viewType !== "source") {
       new Notice("❌ Can only be used during editing");
       return;
     }
